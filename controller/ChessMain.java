9f1cd5d9962d61449c8174573ca1e163092df9e1
Brayan.munoz <brayan.munoz@alignet.com>
2023-02-27 17:48:11 -0500

    asdasd

---
+++ controller/ChessMain.java
@@ -0,0 +1,29 @@
+package controller;
+import javax.swing.*;
+
+import view.ChessPanel;
+// -------------------------------------------------------------------------
+/**
+ * Shows the GUI for the Chess game.
+ * 
+ * @author Ben Katz (bakatz)
+ * @author Myles David II (davidmm2)
+ * @author Danielle Bushrow (dbushrow)
+ * @version 2010.11.17
+ */
+public class ChessMain{
+    // ----------------------------------------------------------
+    /**
+     * Creates the GUI for Chess.
+     * 
+     * @param args
+     *            command line arguments, not used
+     */
+    public static void main( String[] args ){
+        JFrame frame = new JFrame( "YetAnotherChessGame 1.0" );
+        frame.setDefaultCloseOperation( javax.swing.WindowConstants.EXIT_ON_CLOSE );
+        frame.getContentPane().add( new ChessPanel() );
+        frame.pack();
+        frame.setVisible( true );
+    }
+}
